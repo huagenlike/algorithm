bbf154de32b606fc37726a65dc07a03f3f0300ae
may <34886711@qq.com>
2019-11-15 10:57:20 +0800

    有效的括号

--- src/main/java/com/mzl/easy/IsValid.java
+++ src/main/java/com/mzl/easy/IsValid.java
@@ -2,6 +2,8 @@ package com.mzl.easy;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Stack;
 
 /**
  * @ClassName IsValid
@@ -36,32 +38,74 @@ import java.util.Map;
  * 示例 5:
  * 输入: "{[]}"
  * 输出: true
+ *
+ * 示例 5:
+ * 输入: "()(([[{}{}([{}])]]))"
+ * 输出: true
  */
 public class IsValid {
     public static void main(String[] args) {
-//        boolean b = myDemo("})({");
-//        boolean b = myDemo("{[)}");
-        boolean b = myDemo("()[]{}");
+        boolean b = myDemo1("()()({{[[][]{}{([])}]}})");
+//        boolean b = myDemo("{()}");
+//        boolean b = myDemo("()[]{}");
         System.out.println(b);
     }
 
+    /**
+     * 判断是否为空
+     * stack.empty()
+     * 取栈顶值（不出栈）
+     * stack.peek()
+     * 进栈
+     * stack.push(Object);
+     * 出栈
+     * stack.pop();
+     * @param s
+     * @return
+     */
     public static boolean myDemo(String s) {
-        int start = 0;
-        int end = s.length() - 1;
-        while (start < end) {
-            char preChar = s.charAt(start);
-            if(preChar == ')' || preChar == '}' || preChar == ']' || preChar != getValue(s.charAt(end))) return false;
-            start++;
-            end--;
+        if(s.length() == 0) return true;
+        Stack stack=new Stack();
+        stack.push(s.charAt(0));
+        for (int i  = 1; i < s.length(); i++) {
+            if(!stack.empty() && Objects.equals(stack.peek(), getValue(s.charAt(i)))){
+                stack.pop();
+                continue;
+            }
+            stack.push(s.charAt(i));
         }
-        return true;
+        return stack.empty() ? true : false;
+    }
+
+    public static boolean myDemo1(String s) {
+
+        Stack<Character> stack = new Stack<Character>();
+
+        Map<Character, Character> map = new HashMap<>();
+        map.put('}', '{');
+        map.put(')', '(');
+        map.put(']', '[');
+
+        for (int i  = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(map.containsKey(c)){
+                //堆栈的顶部。如果堆栈为空,设置一个虚拟的“#”
+                char topElement = stack.isEmpty() ? '#' : stack.pop();
+                if(topElement != map.get(c)) return false;
+            }else{
+                stack.push(c);
+            }
+
+        }
+        return stack.empty() ? true : false;
     }
 
     public static char getValue(char str){
         Map<Character, Character> map = new HashMap<>();
-        map.put('{', '}');
-        map.put('(', ')');
-        map.put('[', ']');
+        //其实只要判断后面的括号
+//        map.put('{', '}');
+//        map.put('(', ')');
+//        map.put('[', ']');
         map.put('}', '{');
         map.put(')', '(');
         map.put(']', '[');
---
+++ src/main/java/com/mzl/easy/MergeTwoLists.java
@@ -0,0 +1,34 @@
+package com.mzl.easy;
+
+/**
+ * @ClassName MergeTwoLists
+ * @Description: 合并两个有序链表
+ * @Author may
+ * @Date 2019/11/15 10:47
+ *
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ */
+public class MergeTwoLists {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static ListNode myDemo(ListNode listNode1, ListNode listNode2) {
+        return null;
+    }
+}
+
+class ListNode{
+    int val;
+    ListNode nextNode;
+    ListNode(int val){
+        this.val=val;
+        this.nextNode=null;
+    }
+}
